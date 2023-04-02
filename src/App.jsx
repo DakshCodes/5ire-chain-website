721cfe272fd7cb9bbbeba84a2855943a87d06199
DakshCodes <honeysingh617522@gmail.com>
2023-04-02 16:20:57 +0530

    navbar starting file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react"
+import SideNav from "./Components/SideNav/SideNav"
+import './index.css';
+import '../src/Components/SideNav/SideNav.css';
 
 function App() {
 
   return (
-   <></>
+   <>
+   <SideNav />
+   </>
   )
 }
 
