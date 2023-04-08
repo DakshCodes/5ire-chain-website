eafa9c3a24cc40d5c232f2d3d2d7fe5932b2162a
DakshCodes <honeysingh617522@gmail.com>
2023-04-08 11:42:45 +0530

    Home Page Completed

--- src/Components/SideNav/SideNav.jsx
+++ src/Components/SideNav/SideNav.jsx
@@ -1,43 +1,64 @@
-import React from 'react'
-import {AiOutlineHome} from 'react-icons/ai'
+import React, { useState } from 'react'
+import { AiOutlineHome, AiOutlineInfoCircle } from 'react-icons/ai'
+import { BsPinAngle } from 'react-icons/bs'
+// import {IoPeopleOutline} from 'react-icons/io'
+import { TbReportSearch } from 'react-icons/tb'
+import { GrValidate } from 'react-icons/gr'
+import { FaEnvira } from 'react-icons/fa'
+
 const SideNav = () => {
+  const [toggle, setToggle] = useState(1);
+  // const toggle=1;
+
   return (
-    <div className='flex border-y-2 h-[100vh] items-center'>
-      <ul>
+    <div className='flex z-50'>
+      <ul className=' fixed z-50 left-0 top-[25%]'>
         <li>
-          <a class='list-item' href=''>
-            {/* <i class='icon-reorder'></i> */}
+          <a onClick={(e) =>{e.preventDefault();
+           setToggle(1)}}
+            class={`list-item  ${toggle === 1 ? '!bg-[#26a0a0] !w-[9em] !text-[#fffcfb] !translate-x-[2em]' : 'bg-black'}`} href=''>
             <AiOutlineHome />
+            <span className={toggle === 1 ? '!block' : ''}>HOME</span>
           </a>
         </li>
         <li>
-          <a class='list-item' href=''>
-            <i class='icon-th-large'></i>
+          <a onClick={(e) =>{e.preventDefault(); setToggle(2)}} className={`list-item  ${toggle === 2 ? '!bg-[#26a0a0] !w-[9em] !text-[#fffcfb] !translate-x-[2em]' : 'bg-black'}`} href=''
+
+          >
+            <AiOutlineInfoCircle />
+            <span className={toggle === 2 ? '!block' : ''}>ABOUT</span>
           </a>
         </li>
         <li>
-          <a class='list-item' href=''>
-            <i class='icon-bar-chart'></i>
+          <a onClick={(e) =>{e.preventDefault(); setToggle(3)}} class='list-item' href=''
+            className={`list-item  ${toggle === 3 ? '!bg-[#26a0a0] !w-[9em] !text-[#fffcfb] !translate-x-[2em]' : 'bg-black'}`}
+          >
+            <BsPinAngle />
+            <span className={toggle === 3 ? '!block' : ''}>TESTNET</span>
           </a>
         </li>
         <li>
-          <a class='list-item' href=''>
-            <i class='icon-tasks'></i>
+          <a onClick={(e) =>{e.preventDefault(); setToggle(4)}} class='list-item' href=''
+            className={`list-item  ${toggle === 4 ? '!bg-[#26a0a0] !w-[9em] !text-[#fffcfb] !translate-x-[2em]' : 'bg-black'}`}
+          >
+            <TbReportSearch />
+            <span className={toggle === 4 ? '!block' : ''}>RESEARCH</span>
           </a>
         </li>
         <li>
-          <a class='list-item' href=''>
-            <i class='icon-bell'></i>
+          <a onClick={(e) =>{e.preventDefault(); setToggle(5)}} class='list-item' href=''
+            className={`list-item  ${toggle === 5 ? '!bg-[#26a0a0] !w-[9em] !text-[#fffcfb] !translate-x-[2em]' : 'bg-black'}`}
+          >
+            <GrValidate />
+            <span className={toggle === 5 ? '!block' : ''}>VALIDATOR</span>
           </a>
         </li>
         <li>
-          <a class='list-item' href=''>
-            <i class='icon-archive'></i>
-          </a>
-        </li>
-        <li>
-          <a class='list-item' href=''>
-            <i class='icon-comment'></i>
+          <a onClick={(ee) =>{e.preventDefault(); setToggle(6)}} class='list-item' href=''
+            className={`list-item  ${toggle === 6 ? '!bg-[#26a0a0] !w-[9em] !text-[#fffcfb] !translate-x-[2em]' : 'bg-black'}`}
+          >
+            <FaEnvira />
+            <span className={toggle === 6 ? '!block' : ''}>SUSTAIN</span>
           </a>
         </li>
       </ul>
---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from '../Components/Home/Home'
+import SideNav from '../Components/SideNav/SideNav'
+const Landing = () => {
+    return (
+        <>
+          <SideNav  />
+            <Home />
+        </>
+    )
+}
+
+export default Landing
