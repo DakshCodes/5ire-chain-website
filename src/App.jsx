93a76eb5452d03527b05dc77adcbcdfd7fd31f4c
Aman Kumar Bairagi <amanbairagi1089@gmail.com>
2023-04-09 16:27:01 +0530

    BuildToEarn and Research pages  added

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,19 @@ import '../src/Components/Home/Home.css';
 import '../src/Components/Sustainbility/Sustainability.css';
 import Landing from "./Pages/Landing";
 import Sustainability from './Components/Sustainbility/Sustainbility'
+import BuildEarn from "./Components/BuildEarn/BuildEarn";
+import TestNet from "../src/Components/TestNet/TestNet"
+import Research from "./Components/Research/Research";
 
 function App() {
 
   return (
    <>
     {/* <Sustainability /> */}
-    <Landing />
+    {/* <BuildEarn /> */}
+    {/* <TestNet /> */}
+    {/* <Landing /> */}
+    <Research />
    </>
   )
 }
